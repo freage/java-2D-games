3063f94fec730096790a129573e960e057dd86a8
Freddie Agestam <frag42iv@gmail.com>
2019-05-23 00:03:29 +0200

    Indentation

--- src/games/MatrixObserverInterface.java
+++ src/games/MatrixObserverInterface.java
@@ -2,9 +2,9 @@ package games;
 
 public interface MatrixObserverInterface {
 
-        /** The BaseModel calls this when updating any entry in its matrix. */
-        void updateSquare(int m, int n, int number);
+    /** The BaseModel calls this when updating any entry in its matrix. */
+    void updateSquare(int m, int n, int number);
 
-        /** Only called when the Controller starts/restarts the game. */
-        void updateMatrix();
+    /** Only called when the Controller starts/restarts the game. */
+    void updateMatrix();
 }
--- src/games/Position.java
+++ src/games/Position.java
@@ -1,19 +1,17 @@
 package games;
 
 public class Position {
-        public int m;
-        public int n;
+    public int m;
+    public int n;
 
-        public Position(){
-                m = 0;
-                n = 0;
-        }
+    public Position(){
+        m = 0;
+        n = 0;
+    }
 
-        public Position(int M, int N){
-                m = M;
-                n = N;
-        }
-
-
+    public Position(int M, int N){
+        m = M;
+        n = N;
+    }
 
 }
